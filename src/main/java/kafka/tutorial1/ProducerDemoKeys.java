37f9ab86a32cdd977986e47ad562c64ca54e56e1
Roman Klauser <roman.klauser.email@gmail.com>
2018-10-05 13:29:26 +0200

    producer with keys

---
+++ src/main/java/kafka/tutorial1/ProducerDemoKeys.java
@@ -0,0 +1,53 @@
+package kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.UUID;
+
+public class ProducerDemoKeys {
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // 1. producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // 2. create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // 3. create ProducerRecord
+        String key = "id_0123";
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", key, "hello, kafka with key");
+
+        // 4. send date - asynchronous
+        producer.send(record, new Callback() {
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                // every time record is successfully sent or an exception is thrown
+                if (e == null) {
+                    logger.info("Metadata:\n" +
+                            "Topic: " + recordMetadata.topic() + "\n" +
+                            "Partition: " + recordMetadata.partition() + "\n" +
+                            "Offset: " + recordMetadata.offset() + "\n" +
+                            "Timestamp: " + recordMetadata.timestamp() + "\n");
+                } else {
+                    logger.error("Error while producing" + e);
+                }
+            }
+        });
+
+        // flash data
+        producer.flush();
+
+        // flash and close
+        producer.close();
+    }
+}
